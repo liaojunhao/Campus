6fc82ae672da116555ba8cec6b0db368c7f0d656
Andyjhl <460200123@qq.com>
2017-08-01 17:20:11 +0800

    campus6

--- src/page/list/containers/Home/home.js
+++ src/page/list/containers/Home/home.js
@@ -8,6 +8,9 @@ import './home.less';
 
 class Home extends Component {
 
+    componentWillMount() {
+      // 组件初始化时只调用，以后组件更新不调用，整个生命周期只调用一次，此时可以修改state
+    }
     render(){
         return (
             <div>
